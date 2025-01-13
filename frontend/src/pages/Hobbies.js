d6da4d35ae006444d619d6b80124efe558eeb612
zoeLamothe <140864713+zoeLamothe@users.noreply.github.com>
2025-01-13 18:12:12 -0500

    Style Loisirs

--- frontend/src/pages/Hobbies.js
+++ frontend/src/pages/Hobbies.js
@@ -1,18 +1,29 @@
 import React from 'react';
-import './CSS/CV.css';  
-
+import './CSS/CV.css';
+import e from './images/Escalade.jfif';
+import e1 from './images/Escalade1.jfif';
+import e2 from './images/Escalade2.jfif';
 
 function Hobbies() {
   return (
-    <div>
-       <section>
-          <h2>Loisirs et intérêts</h2>
-          <ul>
-            <li>Escalade, Camping, Ski, Jeux de société</li>
-          </ul>
-        </section>
+    <div className="hobbies-container">
+      <section className="hobbies-section">
+        <h2 className="section-title">Loisirs et intérêts</h2>
+        <ul className="hobbies-list">
+          <li className="hobby-item">
+            <h3>Escalade</h3>
+            <div className="hobby-images">
+              <img src={e} alt="Escalade 1" className="hobby-image" />
+              <img src={e1} alt="Escalade 2" className="hobby-image" />
+              <img src={e2} alt="Escalade 3" className="hobby-image" />
+            </div>
+          </li>
+          <li className="hobby-item">Camping</li>
+          <li className="hobby-item">Ski</li>
+          <li className="hobby-item">Jeux de société</li>
+        </ul>
+      </section>
     </div>
-
   );
 }
 
